ed63e78024e8b8fd53c9015cae70a2ea0198fae1
ibenamara <benamara.issam@orange.fr>
2020-11-08 19:39:32 +0100

    Add FindNetworkEndPoint Function

--- Network.Nodes.Test/NetworkServiceTest.cs
+++ Network.Nodes.Test/NetworkServiceTest.cs
@@ -17,10 +17,10 @@ namespace Network.Nodes.Test
         [TestCase(3, new int[] { 7, 1, 3, 4, 2, 6, 9 }, new int[] { 3, 3, 4, 6, 6, 9, 5 }, 5)]
         [TestCase(1, new int[] { 7, 1, 3, 4, 2, 6, 9 }, new int[] { 3, 3, 4, 6, 6, 9, 5 }, 5)]
         [TestCase(1, new int[] { 1, 3, 4, 7}, new int[] { 3, 4, 7, 4 }, 7)]
-        public void FindNetworkEndPoint_Test(int startNodeId, int[] fromIds, int[] toIds, int lastIndex)
+        public void FindNetworkEndPoint_Test(int startNodeId, int[] fromIds, int[] toIds, int lastNodeId)
         {
            var result = _networkService.FindNetworkEndPoint(startNodeId, fromIds, toIds);
-           Assert.AreEqual(result, lastIndex);
+           Assert.AreEqual(result, lastNodeId);
         }
     }
 }

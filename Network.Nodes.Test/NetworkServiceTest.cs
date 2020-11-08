92d752e01e8d11f941355d7577416b750079f8e0
ibenamara <benamara.issam@orange.fr>
2020-11-08 19:28:35 +0100

    Add FindNetworkEndPoint Function

---
+++ Network.Nodes.Test/NetworkServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Network.Nodes.Test
+{
+    [TestFixture]
+    public class NetworkServiceTest
+    {
+        private NetworkService _networkService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _networkService = new NetworkService();
+        }
+
+        [TestCase(2, new int[] { 7, 1, 3, 4, 2, 6, 9 }, new int[] { 3, 3, 4, 6, 6, 9, 5 }, 5)]
+        [TestCase(3, new int[] { 7, 1, 3, 4, 2, 6, 9 }, new int[] { 3, 3, 4, 6, 6, 9, 5 }, 5)]
+        [TestCase(1, new int[] { 7, 1, 3, 4, 2, 6, 9 }, new int[] { 3, 3, 4, 6, 6, 9, 5 }, 5)]
+        [TestCase(1, new int[] { 1, 3, 4, 7}, new int[] { 3, 4, 7, 4 }, 7)]
+        public void FindNetworkEndPoint_Test(int startNodeId, int[] fromIds, int[] toIds, int lastIndex)
+        {
+           var result = _networkService.FindNetworkEndPoint(startNodeId, fromIds, toIds);
+           Assert.AreEqual(result, lastIndex);
+        }
+    }
+}
---
+++ Network.Nodes/NetworkService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Network.Nodes
+{
+    public class NetworkService
+    {
+        /// <summary>
+        /// Find Network EndPoint
+        /// </summary>
+        /// <param name="startNodeId"></param>
+        /// <param name="fromIds"></param>
+        /// <param name="toIds"></param>
+        /// <returns name="lastIndex"></returns>
+        public int FindNetworkEndPoint(int startNodeId, int[] fromIds, int[] toIds)
+        {
+            // Index Of StartNodeId from fromIds
+            var startNodeindex = Array.FindIndex(fromIds, index => index == startNodeId);
+           
+            // If Not Found StartNodeId
+            if (startNodeindex < 0)
+            {
+                throw new ArgumentException(nameof(startNodeId));
+            }
+
+            // Get NextNodeId from toIds
+            var nextNodeId = toIds[startNodeindex];
+
+            // Check for NextNodeId index from fromIds
+            var NextNodeIndex = Array.FindIndex(fromIds, index => index == nextNodeId);
+
+            // Check If Recursive loop
+            if (NextNodeIndex > 0 && toIds[NextNodeIndex] == startNodeId)
+            {
+                return toIds[startNodeindex];
+            }
+
+            // return lastIndex
+            var lastIndex = FindNetworkEndPoint(nextNodeId, fromIds, toIds);
+
+            return lastIndex;
+
+        }
+    }
+}
